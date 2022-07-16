87d4829546d74b3f72634d84d8174612adf2fccb
You Song Hou <yousonghou@gmail.com>
2022-07-15 22:11:53 -0400

    feat: install 'react-router-dom'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
 import TableLand from './components/TableLand';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-      <TableLand />
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route
+          path="/test"
+          element={
+            <TableLand /> } />
+        <Route
+          path="/"
+          element={
+            <h1>Home</h1>} />
+      </Routes>
+    </HashRouter>
   );
 }
 
