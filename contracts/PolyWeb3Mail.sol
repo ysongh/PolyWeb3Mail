eedfcda2d487a3cc1cc3a34b92d2d57304fe3fdd
You Song Hou <yousonghou@gmail.com>
2022-11-14 23:19:59 -0500

    feat: add 'addAddressToSend' function to the contract

--- contracts/PolyWeb3Mail.sol
+++ contracts/PolyWeb3Mail.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.9;
 
 contract PolyWeb3Mail {
     mapping(address => string) public addressToTablename;
+    mapping(address => address[]) public allowSendEMailAddresses;
 
     event NewTablename (address owner, string tablename);
 
     function setTablename(string memory _tablename) external {
         addressToTablename[msg.sender] = _tablename;
         emit NewTablename(msg.sender, _tablename);
+    }
+
+    function addAddressToSend(address _to) external {
+        allowSendEMailAddresses[msg.sender].push(_to);
     }
 }
